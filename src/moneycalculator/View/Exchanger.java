051469e38e6d4f9f626af001c185914f116321dd
Hector Alejandro Valido Cabrera <>
2014-10-25 14:06:13 +0100

    starting

---
+++ src/moneycalculator/View/Exchanger.java
@@ -0,0 +1,13 @@
+
+package moneycalculator.View;
+
+import moneycalculator.Model.ExchangeRate;
+
+public class Exchanger {
+
+    public Money exchange(Money money,ExchangeRate rate){
+        return money;
+    }
+    
+    
+}
